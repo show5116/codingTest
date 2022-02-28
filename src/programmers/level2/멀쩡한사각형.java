dcfcffe171a4bddec00371f27a582a325aeba644
show5 <show5116@naver.com>
2022-02-28 23:13:14 +0900

    2022-02-28

--- src/programmers/level2/멀쩡한사각형.java
+++ src/programmers/level2/멀쩡한사각형.java
@@ -2,7 +2,7 @@ package programmers.level2;
 
 public class 멀쩡한사각형 {
     public static void main(String[] args) {
-
+        solution(8,12);
     }
 
     public static long solution(int w, int h) {
